293febc58ae0328a8cc2fff090ab53a31ad32540
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 23:42:38 +0100

    committed day14.py file

---
+++ Code/day14.py
@@ -0,0 +1,10 @@
+def sum_array(array):
+    '''
+    A function named sum_array which returns the sum of passed into it.
+
+    Parameter: array, a sequence of element.
+
+    Returns: Sum of the array
+    '''
+
+    return sum(array)
