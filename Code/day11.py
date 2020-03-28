fbc93bc3e97cf8283bff2668685fa77e9bc9d3b4
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 22:56:42 +0100

    Created day11.py file

---
+++ Code/day11.py
@@ -0,0 +1,16 @@
+import datetime
+def futurePast(date="22-02-2020"):
+    '''
+    A function named futurePast which checks if a date is passed.
+
+    Parameter: takes the date which we want to compare with today as a string in the format dd-mm-yyyy
+
+    Return: Passed if date has been passed, otherwse Not Passes
+    '''
+    present_date = datetime.datetime.now() # Presents date
+    if present_date > datetime.datetime.strptime(date, '%d-%m-%Y'): # Compares present_date with the passed date.
+        return "Passed."
+    return "Not Passed"
+
+
+print(futurePast())
