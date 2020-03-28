27fa965be5b4561f09a56ffc25efb2083f7e700c
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 17:56:22 +0100

    Created day0.py file

--- Code/day0.py
+++ Code/day0.py
@@ -1,1 +1,7 @@
-print(5)
+import datetime
+
+def date():
+    day, time = datetime.datetime.now().strftime('%A'), datetime.datetime.now().strftime('%I %p: %M : %S') 
+    return f'Today is : {day}.\nCurrent time is : {time}'
+
+print(date())
