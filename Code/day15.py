4c72250ce4e1f74de035600eaf4beccb82a6033d
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 23:54:27 +0100

    committed day15.py file

---
+++ Code/day15.py
@@ -0,0 +1,13 @@
+def sentenceBuilder(data):
+    '''
+    A function named sentenceBuilder which forms a sentence from a the value in a list of dict
+
+    Parameter: an array which contains dictionaries, these dictionaries contains only one key: value pair
+
+    Returns: each of the values in a sentence form.
+    '''
+
+    list_ = [name['name'] for name in data] # Gets each value pair
+    last = list_.pop() # Save the last element in the list and removs it from the list.
+    return ', '.join(list_) + ' and ' + last # Turns the list to a string and do some stufss then return it.
+
