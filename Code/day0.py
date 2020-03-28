9d17b2d86ee724aec12cd6d6ab9d141c928c5f02
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 18:06:48 +0100

    Created day0.py file

--- Code/day0.py
+++ Code/day0.py
@@ -1,6 +1,6 @@
 import datetime
 
-def date():
+def code_watch():
     '''
     A function named date
 
@@ -11,4 +11,3 @@ Parameters: None, the function doesn't take in any function.
     day, time = datetime.datetime.now().strftime('%A'), datetime.datetime.now().strftime('%I %p: %M : %S') 
     return f'Today is : {day}.\nCurrent time is : {time}'
 
-print(date())
