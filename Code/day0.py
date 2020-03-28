47037318ae3eaa07c7609cedd00a08ef9dbd89bb
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 17:59:05 +0100

    Updated day0.py file

--- Code/day0.py
+++ Code/day0.py
@@ -1,6 +1,13 @@
 import datetime
 
 def date():
+    '''
+    A function named date
+
+Parameters: None, the function doesn't take in any function.
+
+    Returns: the day, hour, minute and second in a specified format.
+    '''
     day, time = datetime.datetime.now().strftime('%A'), datetime.datetime.now().strftime('%I %p: %M : %S') 
     return f'Today is : {day}.\nCurrent time is : {time}'
 
