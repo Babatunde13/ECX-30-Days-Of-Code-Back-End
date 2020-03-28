6ebf6be26b2d20370c233549dadfe828e4ef0acf
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 19:56:54 +0100

    modified day7.py file

---
+++ Code/day7.py
@@ -0,0 +1,11 @@
+def stringSplitter(string):
+    '''
+    A function named stringSplitter which turns a string to a list.
+
+    Parameter: string, a text which is of type string.
+
+    Return: a list which contains all the elements in the string.
+    '''
+    return [j for j in string]  # Returns each element in a string as a list.
+
+print(stringSplitter('str'))
