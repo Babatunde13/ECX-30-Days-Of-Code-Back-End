c811a60a66d0dcac3e97e399b57b2f8fe452a520
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 18:54:04 +0100

    Created day4.py file

---
+++ Code/day4.py
@@ -0,0 +1,10 @@
+def intersectionEye(list1, list2):
+    ''''
+    A function named intersectionEye which checks for unique elements in two lists
+
+    Parameter: Two lists, list1 and list2
+
+    Returns:  The unique element(s) in both lists as an array.
+    '''
+    return [x for x in list1 if x in list2]
+
